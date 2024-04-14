292fe755f9e9254063638db9f29b9e09035edf78
ahmad <ahmad.jamalelden@gmail.com>
2024-04-14 17:51:46 +0200

    Add_SummayFlowModel

---
+++ BookSpliter2.py
@@ -0,0 +1,132 @@
+import glob
+import os
+import nltk
+
+no_content_table = 0
+
+def split_book(book, paragraph_max_words = 1024):
+    #print("Max size of paragraph is ", paragraph_max_words)
+    book_paragraphs = []
+
+    # Split the content into sentences
+    sentences = nltk.sent_tokenize(book)
+
+    # Delete sentences that contains stop words
+    stop_words = ["Copyright", "Table Of Contents", "title Page", "All rights reserved", "www.", ".org", "@", "e-books", "®", "ISBN:", "©",".com "]
+    hasContenttable = False
+    deleted_sentences = []
+    for j in range(len(sentences)):
+
+        # Check if it is the table of content
+        words = nltk.word_tokenize(sentences[j])
+        chapter_counter = sum(1 for word in words if word.lower() == "chapter")
+        stave_counter = sum(1 for word in words if word.lower() == "stave")
+        part_counter = sum(1 for word in words if word.lower() == "part")
+        digits_counter = sum(1 for word in words if word in ["three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"])
+        number_counter = sum(1 for word in words if word in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+        romanian_counter = sum(1 for word in words if word in ["II", "III", "IV", "VI", "VII", "XII"])
+
+        if chapter_counter > 3 or stave_counter > 3 or part_counter > 8 or digits_counter > 10 or number_counter > 10 or romanian_counter > 6:
+            deleted_sentences.append(j)
+            hasContenttable = True
+            #print("------------------------------------------------------------------------------------------")
+            #print("Book Name: ", os.path.splitext(os.path.basename(book_path))[0])
+            #print("Deleted sentence : ")
+            #print()
+            #print(sentences[j])
+            #print()
+            #print("It's Table Of Contens")
+            #print("------------------------------------------------------------------------------------------")
+
+            # Create the table_contents folder if it doesn't exist
+            #table_contents_path = "D:\\Gethub\\SummaryFlow\\TableContent"
+            #os.makedirs(table_contents_path, exist_ok=True)
+
+            # Write the sentence to a text file
+            #output_filename = os.path.join(table_contents_path, os.path.splitext(os.path.basename(book_path))[0] + ".txt")
+            #with open(output_filename, 'a', encoding='utf-8') as output_file:
+            #    output_file.write(sentences[j] + "\n")
+
+        for i in range(len(stop_words)):
+            if stop_words[i].lower() in sentences[j].lower() and j not in deleted_sentences:
+                deleted_sentences.append(j)
+                #print("------------------------------------------------------------------------------------------")
+                #print("Deleted sentence : ")
+                #print("################################################################")
+                #print(sentences[j])
+                #print("################################################################")
+                #print("It's Stop word : ")
+                #print(stop_words[i])
+                #print("------------------------------------------------------------------------------------------")
+
+    # Calculate number of books that has no content table
+    global no_content_table
+    if hasContenttable == False:
+        no_content_table += 1
+        #print("------------------------------------------------------------------------------------------")
+        #print("Book Name:  (", os.path.splitext(os.path.basename(book_path))[0], ")  Does not has Table Of Content")
+
+    # Delete sentences from the end to the beginning
+    for i in range(len(deleted_sentences)):
+        del sentences[deleted_sentences[len(deleted_sentences) - (i + 1)]]
+
+
+    #print("length of sentences after deletion : ", len(sentences))
+
+    # Initialize variables
+    current_paragraph = []
+    paragraph_count = 0
+    output_path = "D:\\Gethub\\SummaryFlow\\SplitedNovels\\"
+    output_folder = output_path + "book_name" + " paragraphs"
+
+    # Create a new folder to store the paragraph
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Iterate through each sentence
+    for sentence in sentences:
+        words = sentence.split()
+        # Check if adding the current sentence exceeds the word limit
+        if len(current_paragraph) + len(words) <= paragraph_max_words:
+            current_paragraph.extend(words)
+        else:
+            # Save the current paragraph as a separate text file
+            paragraph_count += 1
+            paragraph_filename = os.path.join(output_folder, f"{paragraph_count}.txt")
+            with open(paragraph_filename, 'w', encoding='utf-8') as f:
+                f.write(' '.join(current_paragraph))
+            
+            # Add the paragraph to array of paragraphs
+            ready_paragraph = ' '.join(current_paragraph)
+            book_paragraphs.append(ready_paragraph)
+            # Reset current paragraph
+            current_paragraph = words
+
+    # Save the last paragraph as a separate text file
+    if current_paragraph:
+        paragraph_count += 1
+        paragraph_filename = os.path.join(output_folder, f"{paragraph_count}.txt")
+        with open(paragraph_filename, 'w', encoding='utf-8') as f:
+            f.write(' '.join(current_paragraph))
+        
+        # Add the paragraph to array of paragraphs
+        ready_paragraph = ' '.join(current_paragraph)
+        book_paragraphs.append(ready_paragraph)
+
+    print("Splited successfully !!")
+    return book_paragraphs
+
+
+#book = r"D:\Gethub\SummaryFlow\WholeNovels\animal_farm.txt"
+#book_splits = split_book(book)
+#print ("Number of paragraphs is ", len(book_splits))
+#print (book_splits[0])
+
+# Define the folder containing the text files
+#folder_path = r"D:\Gethub\SummaryFlow\WholeNovels"
+
+# Loop through all the text files in the folder
+#for file_path in glob.glob(os.path.join(folder_path, "*.txt")):
+    # Pass each file's path to the process_file function
+#    split_book(file_path)
+#print("Number of books without content table is ", no_content_table)
+#print("Number of books with content table is ", (284 - no_content_table))
--- yyy.py
+++ yyy.py
@@ -1,30 +1,15 @@
-# Check if the number is divisible by 2 or 3
-def divide_number(number):
-    if number == 1 or number == 2 or number == 3:
-        return number,  # Return a tuple with a single element
-    if number % 3 == 0 or number % 2 == 0:
-        return number,  # Return a tuple with a single element
-    else:
-        # Start values
-        large_part = number - 2
-        small_part = 2
-        # Find two valuse divisible by 2 or 3 there sum equals the input number
-        while True:
-            if (large_part % 3 == 0 or large_part % 2 == 0) and (small_part % 3 == 0 or small_part % 2 == 0):
-                # The two values were found
-                break
-            else:
-                large_part -= 1
-                small_part += 1
-        return large_part, small_part
+summary_list = ["the boy is good", "the girl is good", "the dog is good"]
 
 
-# Test the function with numbers from 1 to 1000
-for x in range(1, 1001):
-    result = divide_number(x)
-    if len(result) == 1:
-        num1 = result[0]
-        print(f"The number {num1} is divisible by 2 or 3")
-    else:
-        num1, num2 = result
-        print(f'The number {x} can be divided into: {num1} and {num2}')
+summarized_book = []
+# Loop through summaries
+#for i in range(len(summary_list)):
+#    # Combine all summarise into a single paragraph
+#    combined_paragraph = ''.join(summary_list)
+#    # Append the combined paragraph to the new book list
+#    summarized_book.append(combined_paragraph)
+result_book = "\n".join(summary_list)
+# Recursively summarize the generated book
+print('A level is summarized successfully!!')
+print('Summarized book : ')
+print(result_book)
